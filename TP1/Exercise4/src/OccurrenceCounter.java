901bf37557a44984c9dfaa03a237fc0380162896
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-10-23 22:38:16 +0100

    Exercise 4 is done

---
+++ TP1/Exercise4/src/OccurrenceCounter.java
@@ -0,0 +1,27 @@
+public class OccurrenceCounter {
+    public int[] nb_Occurence = new int[26];
+    public String ch;
+
+    public OccurrenceCounter(String ch){
+        this.ch = ch;
+    }
+
+    public void operation() {
+        ch = ch.toUpperCase();
+
+        for (int i = 0; i < ch.length(); i++) {
+            char c = ch.charAt(i);
+
+            if (c >= 'A' && c <= 'Z') {
+                nb_Occurence[c - 'A']++;
+            }
+        }
+        System.out.println("La chaîne " + ch + " contient :");
+        for (int i = 0; i < 26; i++) {
+            if (nb_Occurence[i] > 0) {
+                char lettre = (char) (i + 'A');
+                System.out.println(nb_Occurence[i] + " fois la lettre '" + lettre + "'");
+            }
+        }
+    }
+}
