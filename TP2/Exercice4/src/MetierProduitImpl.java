170dccf70241affb0137396a42b361806b72409c
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-10-29 22:45:46 +0100

    setup the project

---
+++ TP2/Exercice4/src/MetierProduitImpl.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class MetierProduitImpl implements IMetierProduit{
+    private List<Produit> produitList;
+    @Override
+    public Produit add(Produit p) {
+        return null;
+    }
+
+    @Override
+    public List<Produit> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Produit> findByNom(String motCle) {
+        return null;
+    }
+
+    @Override
+    public Produit findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
---
+++ TP2/Exercice4/src/Produit.java
@@ -0,0 +1,6 @@
+public class Produit {
+    private Long id;
+    private String nom, marque, description;
+    private Double prix;
+    private int qteStock;
+}
