e55f5abed583b3d51461d72c5b6d62a9535d28dc
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-11-11 18:20:32 +0100

    TP4-Streams, Exercise2 in class Employe add Getters/Setters/Consructors

--- TP4/collections/src/streams/Exercise2/Employe.java
+++ TP4/collections/src/streams/Exercise2/Employe.java
@@ -1,5 +1,39 @@
 package streams.Exercise2;
 
 public class Employe {
+    private String nom, departement;
+    private Double salary;
 
+    public Employe(String nom, String departement, Double salary) {
+        this.nom = nom;
+        this.departement = departement;
+        this.salary = salary;
+    }
+
+    public Employe() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(String departement) {
+        this.departement = departement;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 }
