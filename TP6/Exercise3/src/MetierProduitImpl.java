84d2f54b07184be8f7778207ef1a74af93f4eccd
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-12-06 21:47:07 +0100

    Exercise3 Setup2

---
+++ TP6/Exercise3/src/MetierProduitImpl.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class MetierProduitImpl implements IMetier{
+    @Override
+    public Object add(Object o) {
+        return null;
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public Object findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void delete(String name) {
+
+    }
+
+    @Override
+    public void saveAll() {
+
+    }
+}
