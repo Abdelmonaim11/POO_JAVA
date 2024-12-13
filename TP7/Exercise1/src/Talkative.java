02989a9da7f55eab59430cb35fb89f3f43f550b3
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-12-13 14:43:17 +0100

    Talkative class

---
+++ TP7/Exercise1/src/Talkative.java
@@ -0,0 +1,11 @@
+public class Talkative implements Runnable{
+    private int number;
+    public Talkative(int number){
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
